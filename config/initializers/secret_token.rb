1a0717e1be2d45fd872cc7d1d5a7618bec68826c
wpmslalom <wpmslalom@yahoo.com>
2014-04-24 21:47:29 -0400

    first commit, got it working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testyoutube::Application.config.secret_key_base = 'ed211e9a40cdc662b373ca1edf4167add3cc0058e1ae49b2850b03dd801042f2634e8433f23f55f5bff047bd6e271ea7edb9b8a1afb52630bdaafbd0b47546a8'
